cc7b6689fc73a571f0bae1adc5b14f1e4e9a5c60
Tinotenda Chidawo <tinotendachidawogmailcom@webmail.umm.ac.id>
2025-10-20 01:26:17 +0700

    Updated Main.java output message for Module 3 CODELAB 1

--- module_3/Main.java
+++ module_3/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub! This is Tinotenda’s Module 3 CODELAB 1 program.");
+        System.out.println("Hello GitHub! This is Tinotenda’s updated CODELAB 1 message.");
     }
 }
